21f18b769fd6323d2bf25c424724f1dc9d6a0cbe
Uresha1106220 <ureshapanchal@gmail.com>
2025-06-25 20:36:30 +0530

    ✅ Enabled CORS for frontend access

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app) 
     
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gridtech.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
