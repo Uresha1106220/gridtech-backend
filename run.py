9cb9f7d17117a9cea83138b740166719410dfa0f
Uresha1106220 <ureshapanchal@gmail.com>
2025-06-27 20:20:42 +0530

    Updated product API route and structure

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-    # Use PORT from environment or default to 10000
+    
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=True)
